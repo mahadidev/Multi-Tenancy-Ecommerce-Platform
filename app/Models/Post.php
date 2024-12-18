eb5617d5c8541b2e4209527adb93498a64e94a0c
zakir Hossen <zakirhossen082@gmail.com>
2024-12-18 13:47:48 +0600

    category crud done, post crud 95% done except post published checkbox data read

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'thumbnail',
+        'title',
+        'slug',
+        'content',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
