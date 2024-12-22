c10e3145aedd815b5854e3b7f0fa9cc59622686e
zakir Hossen <zakirhossen082@gmail.com>
2024-12-22 13:11:36 +0600

    add store module, data insert,read,update,delete,search working

---
+++ app/Filament/Seller/Resources/StoreResource/Pages/CreateStore.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Seller\Resources\StoreResource\Pages;
+
+use App\Filament\Seller\Resources\StoreResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateStore extends CreateRecord
+{
+    protected static string $resource = StoreResource::class;
+}
