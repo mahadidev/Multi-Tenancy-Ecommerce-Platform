72a06fb145e6502eaa2b4de23e2432d41e0902bd
yeaminrabbi <yeaminrabbi308@gmail.com>
2024-12-28 15:01:47 +0600

    added request-logs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\Cors;
+use App\Http\Middleware\LogRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(Cors::class);
+        $middleware->append(LogRequests::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
