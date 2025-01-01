e60ebfc0d976ecc2d3da8a78f39a9d62b08cfa4e
prantopodder98 <prantopodder111@gmail.com>
2025-01-01 11:15:31 +0600

    feat: Implement ProductBrand resource with CRUD functionality and slug generation

---
+++ app/Filament/Resources/ProductBrandResource.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductBrandResource\Pages;
+use App\Filament\Resources\ProductBrandResource\RelationManagers;
+use App\Models\ProductBrand;
+use App\Models\Store;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Filters\SelectFilter;
+
+
+class ProductBrandResource extends Resource
+{
+    protected static ?string $model = ProductBrand::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Store Management';
+    protected static ?string $navigationLabel = 'Brands';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make([
+                    Forms\Components\TextInput::make('name')
+                        ->label('Brand Name')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\Select::make('store_id')
+                        ->label('Store')
+                        ->options(Store::all()->pluck('name', 'id'))
+                        ->searchable()
+                        ->required(),
+                ])->columnSpan(6),
+                Forms\Components\Card::make([
+                    Forms\Components\FileUpload::make('image')
+                        ->label('Brand Image')
+                        ->directory('brand-images')
+                        ->disk('public')
+                        ->image()
+                        ->imageEditor()
+                        ->reorderable()
+                        ->appendFiles()
+                        ->openable()
+                        ->downloadable()
+                        ->imageEditorAspectRatios([
+                            '16:9',
+                            '4:3',
+                            '1:1',
+                        ]),
+
+                ])->columnSpan(6),
+            ])->columns(12);
+    }
+
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->searchable()->sortable(),
+                Tables\Columns\ImageColumn::make('image')->label('Brand Image'),
+                Tables\Columns\TextColumn::make('name')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('created_at')->label('Created At')->dateTime('d M, Y'),
+            ])
+            ->filters([
+                SelectFilter::make('store')
+                    ->relationship('store', 'name')
+                    ->searchable()
+                    ->multiple()
+                    ->preload(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProductBrands::route('/'),
+            'create' => Pages\CreateProductBrand::route('/create'),
+            'edit' => Pages\EditProductBrand::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/ProductBrand.php
+++ app/Models/ProductBrand.php
@@ -2,9 +2,42 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductBrand extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'store_id',
+        'image',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($brand) {
+            if (empty($brand->slug)) {
+                $brand->slug = Str::slug($brand->name);
+            }
+        });
+
+        static::updating(function ($brand) {
+            if ($brand->isDirty('name')) {
+                $brand->slug = Str::slug($brand->name);
+            }
+        });
+    }
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class, 'store_id');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'brand_id');
+    }
 }
