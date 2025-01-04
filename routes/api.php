a7d8f62be79c4b6be11b8afb505f6cd7e8f9ff8b
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-01-04 11:43:35 +0600

    add-to-cart

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,9 @@ Route::group(['prefix' => 'v1'], function () {
     // Include the seller routes
     require __DIR__ . '/api/v1/seller.php';
 
+    // Include the site routes
+    require __DIR__ . '/api/v1/site.php';
+
     // Auth Routes
     Route::post('seller/login', [AuthController::class, 'sellerLogin']);
     Route::post('seller/register', [AuthController::class, 'sellerRegister']);
