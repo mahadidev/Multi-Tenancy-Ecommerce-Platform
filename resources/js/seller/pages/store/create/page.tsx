29799d67e25647461649c1daf8252d185e14f2ae
mahadidev <mahadi.dev.pm@gmail.com>
2025-01-04 13:21:15 +0600

    update

--- resources/js/seller/pages/store/create/page.tsx
+++ resources/js/seller/pages/store/create/page.tsx
@@ -1,12 +1,16 @@
 import { BASE_IMAGE_URL } from "@/env";
 import { BlockBreadcrumb } from "@/seller/components/blocks/block-breadcrumb";
 import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
-import React from "react";
+import React, { useState } from "react";
 import { HiLockOpen } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import { ChooseAccountTypeUserOnboarding } from "./account-type";
+import StepOne from "./stepOne";
+import StepTwo from "./stepTwo";
 
 export default function StoreCreatePage() {
+    const [step, setStep] = useState<number>(1);
+
     return (
         <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:min-h-screen">
             <Link
@@ -29,7 +33,7 @@ export default function StoreCreatePage() {
                 />
             </Link>
             <Card
-                className="w-full sm:max-w-screen-sm md:max-w-screen-sm md:flex-row"
+                className="w-full sm:max-w-screen-sm md:max-w-screen-md md:flex-row"
                 theme={{ root: { children: "w-full p-6 sm:p-8 md:p-16" } }}
             >
                 <div className="w-full">
@@ -48,7 +52,7 @@ export default function StoreCreatePage() {
                             </span>
                         </a>
                     </div>
-                    <ol className="mb-6 flex items-center text-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:text-base lg:mb-12">
+                    <ol className="mb-6 flex justify-center items-center text-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:text-base lg:mb-12">
                         <li className="flex items-center text-primary-600 after:mx-6 after:hidden after:h-1 after:w-12 after:border-b after:border-gray-200 dark:text-primary-500 dark:after:border-gray-700 sm:after:inline-block sm:after:content-[''] xl:after:mx-10">
                             <div className="flex items-center after:mx-2 after:after:text-gray-200 after:content-['/'] dark:after:text-gray-500 sm:after:hidden md:flex-col">
                                 <svg
@@ -64,7 +68,7 @@ export default function StoreCreatePage() {
                                     />
                                 </svg>
                                 <span>
-                                    Personal{" "}
+                                    Basic{" "}
                                     <span className="hidden sm:inline-flex">
                                         Info
                                     </span>
@@ -86,160 +90,26 @@ export default function StoreCreatePage() {
                                     />
                                 </svg>
                                 <span>
-                                    Account{" "}
+                                    Store{" "}
                                     <span className="hidden sm:inline-flex">
-                                        Info
+                                        Branding
                                     </span>
                                 </span>
                             </div>
                         </li>
                         <li className="flex items-center sm:block">
                             <div className="mr-2 sm:mx-auto sm:mb-2">3</div>
-                            Confirmation
+                            Theme
                         </li>
                     </ol>
-                    <h1 className="mb-2 text-2xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">
-                        Verify your email address
-                    </h1>
-                    <p className="text-gray-500 dark:text-gray-400">
-                        We emailed you a six-digit code to&nbsp;
-                        <span className="font-medium text-gray-900 dark:text-white">
-                            name@company.com
-                        </span>
-                        . Enter the code below to confirm your email address.
-                    </p>
-                    <form action="#">
-                        <div className="my-4 flex space-x-2 sm:space-x-4 md:my-6">
-                            <div>
-                                <label htmlFor="code-1" className="sr-only">
-                                    First code
-                                </label>
-                                <input
-                                    id="code-1"
-                                    maxLength={1}
-                                    onKeyUp={() =>
-                                        (
-                                            document.querySelector(
-                                                "#code-2"
-                                            ) as HTMLInputElement
-                                        )?.focus()
-                                    }
-                                    required
-                                    type="text"
-                                    className="block h-12 w-12 rounded-lg border border-gray-300 bg-white py-3 text-center text-2xl font-extrabold text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500 sm:h-16 sm:w-16 sm:py-4 sm:text-4xl"
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="code-2" className="sr-only">
-                                    Second code
-                                </label>
-                                <input
-                                    id="code-2"
-                                    maxLength={1}
-                                    onKeyUp={() =>
-                                        (
-                                            document.querySelector(
-                                                "#code-3"
-                                            ) as HTMLInputElement
-                                        )?.focus()
-                                    }
-                                    type="text"
-                                    required
-                                    className="block h-12 w-12 rounded-lg border border-gray-300 bg-white py-3 text-center text-2xl font-extrabold text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500 sm:h-16 sm:w-16 sm:py-4 sm:text-4xl"
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="code-3" className="sr-only">
-                                    Third code
-                                </label>
-                                <input
-                                    type="text"
-                                    maxLength={1}
-                                    id="code-3"
-                                    onKeyUp={() =>
-                                        (
-                                            document.querySelector(
-                                                "#code-4"
-                                            ) as HTMLInputElement
-                                        )?.focus()
-                                    }
-                                    className="block h-12 w-12 rounded-lg border border-gray-300 bg-white py-3 text-center text-2xl font-extrabold text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500 sm:h-16 sm:w-16 sm:py-4 sm:text-4xl"
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="code-4" className="sr-only">
-                                    Fourth code
-                                </label>
-                                <input
-                                    id="code-4"
-                                    maxLength={1}
-                                    onKeyUp={() =>
-                                        (
-                                            document.querySelector(
-                                                "#code-5"
-                                            ) as HTMLInputElement
-                                        )?.focus()
-                                    }
-                                    required
-                                    type="text"
-                                    className="block h-12 w-12 rounded-lg border border-gray-300 bg-white py-3 text-center text-2xl font-extrabold text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500 sm:h-16 sm:w-16 sm:py-4 sm:text-4xl"
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="code-5" className="sr-only">
-                                    Fifth code
-                                </label>
-                                <input
-                                    id="code-5"
-                                    maxLength={1}
-                                    onKeyUp={() =>
-                                        (
-                                            document.querySelector(
-                                                "#code-6"
-                                            ) as HTMLInputElement
-                                        )?.focus()
-                                    }
-                                    required
-                                    type="text"
-                                    className="block h-12 w-12 rounded-lg border border-gray-300 bg-white py-3 text-center text-2xl font-extrabold text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500 sm:h-16 sm:w-16 sm:py-4 sm:text-4xl"
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="code-6" className="sr-only">
-                                    Sixth code
-                                </label>
-                                <input
-                                    type="text"
-                                    maxLength={1}
-                                    id="code-6"
-                                    className="block h-12 w-12 rounded-lg border border-gray-300 bg-white py-3 text-center text-2xl font-extrabold text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500 sm:h-16 sm:w-16 sm:py-4 sm:text-4xl"
-                                    required
-                                />
-                            </div>
-                        </div>
-                        <p className="mb-4 rounded-lg bg-gray-50 p-4 text-sm text-gray-500 dark:bg-gray-900 dark:text-gray-400 md:mb-6">
-                            Make sure to keep this window open while checking
-                            your inbox.
-                        </p>
-                        <div className="grid grid-cols-2 gap-3">
-                            <Button
-                                color="gray"
-                                href="#"
-                                size="xl"
-                                className="hover:bg-gray-100 hover:text-primary-600 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white [&>span]:py-4 [&>span]:text-sm"
-                            >
-                                Prev: Account Info
-                            </Button>
-                            <Button
-                                type="submit"
-                                size="xl"
-                                className="[&>span]:text-sm"
-                            >
-                                Verify account
-                            </Button>
-                        </div>
-                    </form>
+                    <>
+                        {step === 1 && (
+                            <StepOne step={step} setStep={setStep} />
+                        )}
+                        {step === 2 && (
+                            <StepTwo step={step} setStep={setStep} />
+                        )}
+                    </>
                 </div>
             </Card>
         </div>
