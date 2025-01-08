603fe5e9136a2e9b39020bde25eaf7980c851a84
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-01-08 14:54:17 +0600

    add store pages

---
+++ app/Models/StorePageWidget.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StorePageWidget extends Model
+{
+    protected $fillable = [
+        'store_page_id',
+        'name',
+        'label',
+        'inputs'
+    ];
+
+    protected $casts = [
+        'inputs' => 'array',
+    ];
+
+    protected $hidden  = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function page(){
+        return $this->belongsTo(StorePage::class, 'store_page_id');
+    }
+}
