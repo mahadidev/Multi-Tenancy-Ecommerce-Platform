80f0017f55cc4fa01536d95d2c500d326b90ec6e
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-01-15 15:07:05 +0600

    changes

--- resources/js/seller/app.tsx
+++ resources/js/seller/app.tsx
@@ -8,7 +8,7 @@ import OnboardRoutes from "./routes/onboardRoutes";
 const App = () => {
     return (
         <>
-            <BrowserRouter basename={"/seller"}>
+            <BrowserRouter basename="/seller">
                 <Routes>
                     <Route path="onboard" element={<LoggedMiddleware />}>
                         <Route index path="*" element={<OnboardRoutes />} />
@@ -19,9 +19,10 @@ const App = () => {
 
                     <Route path="/" element={<LoggedMiddleware />}>
                         <Route index path="/" element={<DashboardRoutes />} />
-                        <Route index path="*" element={<DashboardRoutes />} />
+                        <Route path="*" element={<DashboardRoutes />} />
                     </Route>
 
+                    {/* Catch-all for undefined routes in React */}
                     <Route path="*" element={<NotFoundPage />} />
                 </Routes>
             </BrowserRouter>
@@ -30,3 +31,37 @@ const App = () => {
 };
 
 export default App;
+
+
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import NotFoundPage from "./pages/landing/notFound/page";
+// import { GuestMiddleware, LoggedMiddleware } from "./protectedRoutes";
+// import DashboardRoutes from "./routes/dashboardRoutes";
+// import GuestRoutes from "./routes/guestRoutes";
+// import OnboardRoutes from "./routes/onboardRoutes";
+
+// const App = () => {
+//     return (
+//         <>
+//             <BrowserRouter basename={"/seller"}>
+//                 <Routes>
+//                     <Route path="onboard" element={<LoggedMiddleware />}>
+//                         <Route index path="*" element={<OnboardRoutes />} />
+//                     </Route>
+//                     <Route path="login" element={<GuestMiddleware />}>
+//                         <Route index path="*" element={<GuestRoutes />} />
+//                     </Route>
+
+//                     <Route path="/" element={<LoggedMiddleware />}>
+//                         <Route index path="/" element={<DashboardRoutes />} />
+//                         <Route index path="*" element={<DashboardRoutes />} />
+//                     </Route>
+
+//                     <Route path="*" element={<NotFoundPage />} />
+//                 </Routes>
+//             </BrowserRouter>
+//         </>
+//     );
+// };
+
+// export default App;
