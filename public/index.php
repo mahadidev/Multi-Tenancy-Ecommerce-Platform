e5253c617e6a7e0e39366d7c55ecd237e21d2fcc
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-01-15 15:21:32 +0600

    updated public/index

--- public/index.php
+++ public/index.php
@@ -1,8 +1,34 @@
 <?php
 
+// use Illuminate\Http\Request;
+
+// define('LARAVEL_START', microtime(true));
+
+// // Determine if the application is in maintenance mode...
+// if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
+
+// // Register the Composer autoloader...
+// require __DIR__.'/../vendor/autoload.php';
+
+// // Bootstrap Laravel and handle the request...
+// (require_once __DIR__.'/../bootstrap/app.php')
+//     ->handleRequest(Request::capture());
+
+
 use Illuminate\Http\Request;
 
+
+// **Add logic to check for blocked routes before bootstrapping**
+$request = Request::capture();
+if (strpos($request->getPathInfo(), 'public') !== false) {
+    abort(404);
+}
+
 define('LARAVEL_START', microtime(true));
+
+
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
@@ -14,4 +40,4 @@ require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+    ->handleRequest($request);
