3f3af91fc93d7ab94814f1eee973c2c0cb45d168
prantopodder98 <prantopodder111@gmail.com>
2025-01-15 11:22:48 +0600

    chore: Refactor FileStorage resource to enhance file handling and add file preview component

--- app/Filament/Resources/FileStorageResource/Pages/CreateFileStorage.php
+++ app/Filament/Resources/FileStorageResource/Pages/CreateFileStorage.php
@@ -11,39 +11,4 @@ use Illuminate\Support\Facades\Storage;
 class CreateFileStorage extends CreateRecord
 {
     protected static string $resource = FileStorageResource::class;
-
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        // Set user_id if not provided
-        if (empty($data['user_id'])) {
-            $data['user_id'] = auth()->id();
-        }
-    
-        // Handle file upload and generate unique name
-        if (isset($data['location'])) {
-            $uniqueName = uniqid() . '_' . time();
-            $extension = pathinfo($data['location'], PATHINFO_EXTENSION);
-            $newFileName = $uniqueName . '.' . $extension;
-            
-            // Rename the file in storage
-            $storage = Storage::disk('public');
-            $oldPath = $data['location'];
-            $newPath = 'file_storage/' . $newFileName;
-            
-            if ($storage->exists($oldPath)) {
-                $storage->move($oldPath, $newPath);
-            }
-            
-            // Update location with new path
-            $data['location'] = $newPath;
-            
-            // Set name field
-            $data['name'] = $newFileName;
-            
-            // Set type based on extension
-            $data['type'] = strtolower($extension) === 'pdf' ? 'pdf' : 'image';
-        }
-    
-        return $data;
-    }
 }
---
+++ resources/views/components/file-preview.blade.php
@@ -0,0 +1,24 @@
+<!-- resources/views/components/file-preview.blade.php -->
+<div>
+    @php
+        $record = $getRecord();
+    @endphp
+    
+    @if($record->type === 'image')
+        <div class="w-20 h-10">
+            <a href="{{ Storage::url($record->location) }}" target="_blank">
+                <img 
+                    src="{{ Storage::url($record->location) }}" 
+                    alt="Image Preview"
+                    class="w-full h-full object-cover rounded-lg"
+                />
+            </a>
+        </div>
+    @else
+    <a href="{{ Storage::url($record->location) }}" target="_blank" class="block w-20 h-10">
+        <div class="w-full h-full flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="none" stroke="#ff2f2f" stroke-width="2" d="M4.998 9V1H19.5L23 4.5V23H4M18 1v5h5M3 12h1.5c2 0 2.25 1.25 2.25 2s-.25 2-2.25 2H3.25v2H3zm6.5 6v-6h1.705c1.137 0 2.295.5 2.295 3s-1.158 3-2.295 3zm7 1v-7h4m-4 3.5h3"/></svg>
+        </div>
+    </a>
+    @endif
+</div>
