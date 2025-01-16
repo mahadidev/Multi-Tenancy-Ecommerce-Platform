390867626f7354a47fdcfdd836853f8ecbbba8e8
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-01-16 16:53:37 +0600

    seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolePermissionSeeder::class,
             UserSeeder::class,
+            PageTypeSeeder::class,
             // SellerAccountSeeder::class,
             // WidgetSeeder::class,
             // ThemeSeeder::class,
