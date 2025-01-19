8c788c1af5801a371c3c0f9ee565bb65854394be
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-01-19 11:29:41 +0600

    updated store pages

--- routes/api/v1/seller.php
+++ routes/api/v1/seller.php
@@ -39,10 +39,11 @@ Route::group(['prefix' => 'seller', 'middleware' => ['auth:sanctum']], function 
     Route::get('logout', [AuthController::class, 'logout']);
 
     // Store Pages Routes
-    Route::get('stores/{store_id}/pages', [StorePageController::class, 'pages']);
+    Route::get('stores/{store_id}/pages', [StorePageController::class, 'index']);
     Route::post('stores/{store_id}/pages/store', [StorePageController::class, 'store']);
     Route::get('stores/{store_id}/pages/{page_id}', [StorePageController::class, 'view']);
-    Route::put('stores/{store_id}/pages/update/{page_id}', [StorePageController::class, 'update']);
+    Route::post('stores/{store_id}/pages/update/{page_id}', [StorePageController::class, 'update']);
+    Route::delete('stores/{store_id}/pages/delete/{page_id}', [StorePageController::class, 'destroy']);
 
     // Store Page Widget Routes
     Route::get('stores/pages/{pageId}/widgets', [StorePageWidgetController::class, 'index']);
