514c73c1c20c0bc197b3a794e5e3d21abff9ae32
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-01-26 10:04:02 +0600

    theme seeder active

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
             RolePermissionSeeder::class,
             UserSeeder::class,
             PageTypeSeeder::class,
-            // ThemeSeeder::class,
+            ThemeSeeder::class,
             // SellerAccountSeeder::class,
             // WidgetSeeder::class,
         ]);
