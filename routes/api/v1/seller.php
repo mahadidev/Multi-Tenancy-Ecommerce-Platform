006500529f986d790619dcd7585193a935b525cf
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-01-27 13:15:21 +0600

    cleanup

--- routes/api/v1/seller.php
+++ routes/api/v1/seller.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\v1\AuthController;
 use App\Http\Controllers\Api\v1\site\ProductReviewController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::group(['prefix' => 'seller', 'middleware' => ['auth:sanctum']], function () {
     // Get owned store list
     Route::get('/get-stores', [StoreController::class, 'index']);
