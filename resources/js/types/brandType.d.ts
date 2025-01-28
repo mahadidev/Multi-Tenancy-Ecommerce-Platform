e5ff1c1fb1986dbbed3e3f756204b276937416f0
MHRafix <fmhrafiz27@gmail.com>
2025-01-27 21:33:18 -0800

    in brand module, brand table cells properties fixed, and create brand form validation + auto slug creation with name and after completed creation form reseting done:-

--- resources/js/types/brandType.d.ts
+++ resources/js/types/brandType.d.ts
@@ -1,7 +1,8 @@
 export interface BrandType {
-	id: number;
-	store_id: number;
-	name: string;
-	slug: string;
-	image: string;
+    id: number;
+    store_id: number;
+    name: string;
+    slug: string;
+    image: string;
+    created_at: string;
 }
