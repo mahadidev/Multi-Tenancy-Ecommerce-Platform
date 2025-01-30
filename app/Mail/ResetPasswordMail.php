985b78ae9ef221793906cf4ff6f77ac30b80a743
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-01-30 12:02:03 +0600

    chore: added email for forget password

---
+++ app/Mail/ResetPasswordMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ResetPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $resetUrl;
+    public $userName;
+
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($resetUrl, $userName)
+    {
+        $this->resetUrl = $resetUrl;
+        $this->userName = $userName;
+    }
+
+    public function build()
+    {
+        return $this->markdown('emails.reset-password')
+            ->subject('Reset Your Password')
+            ->with([
+                'resetUrl' => $this->resetUrl,
+                'userName' => $this->userName,
+            ]);
+    }
+}
