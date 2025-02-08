52fffa6004f16323d5f0bab9b840c58580bbe596
prantopodder98 <prantopodder111@gmail.com>
2025-02-08 16:21:52 +0600

    chore: Add SvgIcon resource with CRUD functionality and migration

---
+++ app/Filament/Resources/SvgIconResource/Pages/CreateSvgIcon.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\SvgIconResource\Pages;
+
+use App\Filament\Resources\SvgIconResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSvgIcon extends CreateRecord
+{
+    protected static string $resource = SvgIconResource::class;
+}
---
+++ resources/views/components/svg-preview.blade.php
@@ -0,0 +1,5 @@
+<div class="px-4 py-3">
+    <div class="w-10 h-10">
+        {!! $getRecord()->icon_preview !!}
+    </div>
+</div>
