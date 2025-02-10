221a5b6ab622f3d1ae4b3f43fdc13888b940eb4f
MHRafix <fmhrafiz27@gmail.com>
2025-02-10 12:49:22 +0600

    update and create customer api done

--- resources/js/seller/hooks/useCustomer.ts
+++ resources/js/seller/hooks/useCustomer.ts
@@ -1,15 +1,106 @@
-import { useFetchCustomersQuery } from "@seller/store/reducers/customerApi";
+import {
+    useCreateCustomerMutation,
+    useFetchCustomersQuery,
+    useUpdateCustomerMutation,
+} from "@seller/store/reducers/customerApi";
 import { useAppSelector } from "@seller/store/store";
+import { CustomerType } from "@type/customersType";
+import useToast from "./useToast";
 
 const useCustomer = () => {
     // fetch customers
     useFetchCustomersQuery();
 
+    const { toaster } = useToast();
+
     // select customers
     const { customers } = useAppSelector((state) => state.customer);
 
+    // create customer
+    const [
+        handleCreateCustomer,
+        {
+            isLoading: isCreateCustomerLoading,
+            isError: isCreateCustomerError,
+            error: createCustomerError,
+            data: createCustomerData,
+        },
+    ] = useCreateCustomerMutation();
+    const createCustomer = ({
+        formData,
+        onSuccess,
+    }: {
+        formData: CustomerType;
+        onSuccess?: CallableFunction;
+    }) => {
+        handleCreateCustomer(formData).then((response) => {
+            if (response.data?.status === 200) {
+                if (onSuccess) {
+                    onSuccess(response.data.data);
+                }
+                toaster({
+                    text: "Customer created successfully",
+                    status: "success",
+                });
+            } else {
+                toaster({
+                    text: "Failed create customer",
+                    status: "error",
+                });
+            }
+        });
+    };
+
+    // update customer
+    const [
+        handleUpdateCustomer,
+        {
+            isLoading: isUpdateCustomerLoading,
+            isError: isUpdateCustomerError,
+            error: updateCustomerError,
+            data: updateCustomerData,
+        },
+    ] = useUpdateCustomerMutation();
+    const updateCustomer = ({
+        formData,
+        onSuccess,
+    }: {
+        formData: CustomerType;
+        onSuccess?: CallableFunction;
+    }) => {
+        handleCreateCustomer(formData).then((response) => {
+            if (response.data?.status === 200) {
+                if (onSuccess) {
+                    onSuccess(response.data.data);
+                }
+                toaster({
+                    text: "Customer updated successfully",
+                    status: "success",
+                });
+            } else {
+                toaster({
+                    text: "Failed update customer",
+                    status: "error",
+                });
+            }
+        });
+    };
     return {
         customers,
+        createCustomer: {
+            submit: createCustomer,
+            isLoading: isCreateCustomerLoading,
+            isError: isCreateCustomerError,
+            error: createCustomerError,
+            data: createCustomerData,
+        },
+        updateCustomer: {
+            submit: updateCustomer,
+            isLoading: isUpdateCustomerLoading,
+            isError: isUpdateCustomerError,
+            error: updateCustomerError,
+            data: updateCustomerData,
+        },
     };
 };
 export default useCustomer;
--- resources/js/seller/store/reducers/customerApi.ts
+++ resources/js/seller/store/reducers/customerApi.ts
@@ -1,7 +1,7 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { PREFIX } from "@seller/seller_env";
 import { ApiResponseType } from "@type/apiType";
-import { CustomerDataType } from "@type/customersType";
+import { CustomerDataType, CustomerType } from "@type/customersType";
 import baseQueryWithReAuth, { createRequest } from "../baseQueryWithReAuth";
 import { setCustomers } from "../slices/customerSlice";
 
@@ -33,7 +33,35 @@ export const customerApi = createApi({
                 });
             },
         }),
+
+        // create customer
+        createCustomer: builder.mutation<ApiResponseType, CustomerType>({
+            query: (formData) =>
+                createRequest({
+                    url: `${PREFIX}/customers`,
+                    method: "post",
+                    body: formData,
+                }),
+            invalidatesTags: ["Customers"],
+            transformErrorResponse: (error: any) => error.data,
+        }),
+        // update customer
+        updateCustomer: builder.mutation<ApiResponseType, CustomerType>({
+            query: (formData) =>
+                createRequest({
+                    url: `${PREFIX}/customers`,
+                    method: "post",
+                    apiMethod: "PUT",
+                    body: formData,
+                }),
+            invalidatesTags: ["Customers"],
+            transformErrorResponse: (error: any) => error.data,
+        }),
     }),
 });
 
-export const { useFetchCustomersQuery } = customerApi;
+export const {
+    useFetchCustomersQuery,
+    useUpdateCustomerMutation,
+    useCreateCustomerMutation,
+} = customerApi;
