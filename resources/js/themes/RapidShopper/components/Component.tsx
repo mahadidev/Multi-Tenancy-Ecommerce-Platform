6dea3cf8c8abd466fcabcaed7324ccc1ac05ac53
Mahadi Hasan <mahadi.dev.pm@gmail.com>
2025-03-04 03:42:07 +0600

    update

--- resources/js/themes/RapidShopper/components/Component.tsx
+++
@@ -1,6 +0,0 @@
-import { ThemeWidgetPropsType } from "@type/themeType";
-import { FC } from "react";
-
-export const Component: FC<ThemeWidgetPropsType> = (props) => {
-    return <div>RapidShopperComponent {props.widget.label}</div>;
-};
