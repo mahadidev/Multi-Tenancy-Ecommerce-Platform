9977b2585c4421b16232943c67261a58dc91e787
MHRafix <fmhrafiz27@gmail.com>
2025-03-11 17:32:39 +0600

    payment proceccisng done

--- resources/js/seller/hooks/usePlan.ts
+++ resources/js/seller/hooks/usePlan.ts
@@ -4,15 +4,12 @@ import {
     useSubscribePlanMutation,
 } from "@seller/store/reducers/subscriptionPlanApi";
 import { useAppSelector } from "@seller/store/store";
-import { useNavigate } from "react-router-dom";
 import useToast from "./useToast";
 
 const usePlans = () => {
     useFetchPlansQuery();
 
     const { toaster } = useToast();
-
-    const navigate = useNavigate();
 
     // select plans
     const { plans } = useAppSelector((state) => state.plans);
