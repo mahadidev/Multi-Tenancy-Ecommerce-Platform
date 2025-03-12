13cd85b4505532ddfa836f99584cfb469b60478b
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-03-12 22:48:59 +0600

    updated pulse migration

--- database/migrations/2024_12_31_165719_create_pulse_tables.php
+++ database/migrations/2024_12_31_165719_create_pulse_tables.php
@@ -18,57 +18,84 @@ return new class extends PulseMigration
         Schema::create('pulse_values', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('timestamp');
-            $table->string('type');
+            $table->string('type', 150)->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
             $table->mediumText('key');
-            match ($this->driver()) {
-                'mariadb', 'mysql' => $table->char('key_hash', 16)->charset('binary')->virtualAs('unhex(md5(`key`))'),
-                'pgsql' => $table->uuid('key_hash')->storedAs('md5("key")::uuid'),
-                'sqlite' => $table->string('key_hash'),
-            };
+            
+            switch ($this->driver()) {
+                case 'mariadb':
+                case 'mysql':
+                    $table->char('key_hash', 32)->charset('binary')->virtualAs('unhex(md5(`key`))');
+                    break;
+                case 'pgsql':
+                    $table->uuid('key_hash')->storedAs('md5("key")::uuid');
+                    break;
+                case 'sqlite':
+                    $table->string('key_hash', 32);
+                    break;
+            }
+            
             $table->mediumText('value');
 
-            $table->index('timestamp'); // For trimming...
-            $table->index('type'); // For fast lookups and purging...
-            $table->unique(['type', 'key_hash']); // For data integrity and upserts...
+            $table->index('timestamp'); 
+            $table->index('type');
+            $table->unique(['type', 'key_hash']);
         });
 
         Schema::create('pulse_entries', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('timestamp');
-            $table->string('type');
+            $table->string('type', 150)->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
             $table->mediumText('key');
-            match ($this->driver()) {
-                'mariadb', 'mysql' => $table->char('key_hash', 16)->charset('binary')->virtualAs('unhex(md5(`key`))'),
-                'pgsql' => $table->uuid('key_hash')->storedAs('md5("key")::uuid'),
-                'sqlite' => $table->string('key_hash'),
-            };
+            
+            switch ($this->driver()) {
+                case 'mariadb':
+                case 'mysql':
+                    $table->char('key_hash', 32)->charset('binary')->virtualAs('unhex(md5(`key`))');
+                    break;
+                case 'pgsql':
+                    $table->uuid('key_hash')->storedAs('md5("key")::uuid');
+                    break;
+                case 'sqlite':
+                    $table->string('key_hash', 32);
+                    break;
+            }
+            
             $table->bigInteger('value')->nullable();
 
-            $table->index('timestamp'); // For trimming...
-            $table->index('type'); // For purging...
-            $table->index('key_hash'); // For mapping...
-            $table->index(['timestamp', 'type', 'key_hash', 'value']); // For aggregate queries...
+            $table->index('timestamp'); 
+            $table->index('type');
+            $table->index('key_hash'); 
+            $table->index(['timestamp', 'type', 'key_hash', 'value']); 
         });
 
         Schema::create('pulse_aggregates', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('bucket');
             $table->unsignedMediumInteger('period');
-            $table->string('type');
+            $table->string('type', 150)->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
             $table->mediumText('key');
-            match ($this->driver()) {
-                'mariadb', 'mysql' => $table->char('key_hash', 16)->charset('binary')->virtualAs('unhex(md5(`key`))'),
-                'pgsql' => $table->uuid('key_hash')->storedAs('md5("key")::uuid'),
-                'sqlite' => $table->string('key_hash'),
-            };
-            $table->string('aggregate');
+            
+            switch ($this->driver()) {
+                case 'mariadb':
+                case 'mysql':
+                    $table->char('key_hash', 32)->charset('binary')->virtualAs('unhex(md5(`key`))');
+                    break;
+                case 'pgsql':
+                    $table->uuid('key_hash')->storedAs('md5("key")::uuid');
+                    break;
+                case 'sqlite':
+                    $table->string('key_hash', 32);
+                    break;
+            }
+            
+            $table->string('aggregate', 150)->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
             $table->decimal('value', 20, 2);
             $table->unsignedInteger('count')->nullable();
 
-            $table->unique(['bucket', 'period', 'type', 'aggregate', 'key_hash']); // Force "on duplicate update"...
-            $table->index(['period', 'bucket']); // For trimming...
-            $table->index('type'); // For purging...
-            $table->index(['period', 'type', 'aggregate', 'bucket']); // For aggregate queries...
+            $table->unique(['bucket', 'period', 'type', 'aggregate', 'key_hash']);
+            $table->index(['period', 'bucket']); 
+            $table->index('type'); 
+            $table->index(['period', 'type', 'aggregate', 'bucket']); 
         });
     }
 
