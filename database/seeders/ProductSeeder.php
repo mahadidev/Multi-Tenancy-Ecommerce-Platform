ed0247e9dd776f8838a6b1f51325d4840c2eafe8
YeaminRabbi <yeaminrabbi308@gmail.com>
2025-03-15 11:44:00 +0600

    updated product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -22,7 +22,9 @@ class ProductSeeder extends Seeder
                 'price' => 999.99,
                 'thumbnail' => 'seeders/products/iphone-14-pro.jpg',
                 'category_slug' => 'electronics',
-                'brand_slug' => 'apple'
+                'brand_slug' => 'apple',
+                'is_trending' => true,
+                'is_featured' => true,
             ],
             [
                 'name' => 'Samsung Galaxy S23',
@@ -31,7 +33,9 @@ class ProductSeeder extends Seeder
                 'price' => 849.99,
                 'thumbnail' => 'seeders/products/samsung-galaxy-s23.jpg',
                 'category_slug' => 'electronics',
-                'brand_slug' => 'samsung'
+                'brand_slug' => 'samsung',
+                'is_trending' => false,
+                'is_featured' => true,
             ],
             [
                 'name' => 'Nike Air Max 270',
@@ -40,7 +44,9 @@ class ProductSeeder extends Seeder
                 'price' => 129.99,
                 'thumbnail' => 'seeders/products/nike-air-max-270.jpg',
                 'category_slug' => 'clothing',
-                'brand_slug' => 'nike'
+                'brand_slug' => 'nike',
+                'is_trending' => false,
+                'is_featured' => false,
             ],
             [
                 'name' => 'Adidas Ultraboost',
@@ -49,7 +55,9 @@ class ProductSeeder extends Seeder
                 'price' => 149.99,
                 'thumbnail' => 'seeders/products/adidas-ultraboost.jpg',
                 'category_slug' => 'clothing',
-                'brand_slug' => 'adidas'
+                'brand_slug' => 'adidas',
+                'is_trending' => false,
+                'is_featured' => false,
             ],
             [
                 'name' => 'Sony WH-1000XM5',
@@ -58,7 +66,9 @@ class ProductSeeder extends Seeder
                 'price' => 299.99,
                 'thumbnail' => 'seeders/products/sony-wh-1000xm5.jpg',
                 'category_slug' => 'electronics',
-                'brand_slug' => 'sony'
+                'brand_slug' => 'sony',
+                'is_trending' => true,
+                'is_featured' => false,
             ],
             [
                 'name' => 'Microsoft Surface Pro 9',
@@ -67,7 +77,9 @@ class ProductSeeder extends Seeder
                 'price' => 1099.99,
                 'thumbnail' => 'seeders/products/microsoft-surface-pro-9.jpg',
                 'category_slug' => 'electronics',
-                'brand_slug' => 'microsoft'
+                'brand_slug' => 'microsoft',
+                'is_trending' => false,
+                'is_featured' => true,
             ],
             [
                 'name' => 'Gucci Leather Bag',
@@ -76,7 +88,9 @@ class ProductSeeder extends Seeder
                 'price' => 1999.99,
                 'thumbnail' => 'seeders/products/gucci-leather-bag.jpg',
                 'category_slug' => 'travel-luggage',
-                'brand_slug' => 'gucci'
+                'brand_slug' => 'gucci',
+                'is_trending' => true,
+                'is_featured' => true,
             ],
             [
                 'name' => 'Puma Running Shoes',
@@ -85,7 +99,9 @@ class ProductSeeder extends Seeder
                 'price' => 89.99,
                 'thumbnail' => 'seeders/products/puma-running-shoes.jpg',
                 'category_slug' => 'clothing',
-                'brand_slug' => 'puma'
+                'brand_slug' => 'puma',
+                'is_trending' => true,
+                'is_featured' => true,
             ],
             [
                 'name' => "L'Oréal Face Cream",
@@ -94,7 +110,9 @@ class ProductSeeder extends Seeder
                 'price' => 29.99,
                 'thumbnail' => 'seeders/products/loreal-face-cream.jpg',
                 'category_slug' => 'beauty-personal-care',
-                'brand_slug' => 'loreal'
+                'brand_slug' => 'loreal',
+                'is_trending' => true,
+                'is_featured' => true,
             ],
             [
                 'name' => 'Coca-Cola Pack of 12',
@@ -103,7 +121,9 @@ class ProductSeeder extends Seeder
                 'price' => 8.99,
                 'thumbnail' => 'seeders/products/coca-cola-pack-12.jpg',
                 'category_slug' => 'groceries',
-                'brand_slug' => 'coca-cola'
+                'brand_slug' => 'coca-cola',
+                'is_trending' => false,
+                'is_featured' => false,
             ],
             [
                 'name' => 'Tesla Model Y',
@@ -112,7 +132,9 @@ class ProductSeeder extends Seeder
                 'price' => 49999.99,
                 'thumbnail' => 'seeders/products/tesla-model-y.jpg',
                 'category_slug' => 'automotive',
-                'brand_slug' => 'tesla'
+                'brand_slug' => 'tesla',
+                'is_trending' => false,
+                'is_featured' => false,
             ],
             [
                 'name' => 'Toyota Corolla 2023',
@@ -121,7 +143,9 @@ class ProductSeeder extends Seeder
                 'price' => 22999.99,
                 'thumbnail' => 'seeders/products/toyota-corolla-2023.jpg',
                 'category_slug' => 'automotive',
-                'brand_slug' => 'toyota'
+                'brand_slug' => 'toyota',
+                'is_trending' => false,
+                'is_featured' => false,
             ],
             [
                 'name' => 'Chanel No. 5 Perfume',
@@ -130,7 +154,9 @@ class ProductSeeder extends Seeder
                 'price' => 129.99,
                 'thumbnail' => 'seeders/products/chanel-no-5.jpg',
                 'category_slug' => 'beauty-personal-care',
-                'brand_slug' => 'chanel'
+                'brand_slug' => 'chanel',
+                'is_trending' => false,
+                'is_featured' => false,
             ],
             [
                 'name' => 'Intel Core i9 Processor',
@@ -139,7 +165,9 @@ class ProductSeeder extends Seeder
                 'price' => 499.99,
                 'thumbnail' => 'seeders/products/intel-core-i9.jpg',
                 'category_slug' => 'electronics',
-                'brand_slug' => 'intel'
+                'brand_slug' => 'intel',
+                'is_trending' => false,
+                'is_featured' => false,
             ],
             [
                 'name' => 'Dell XPS 15',
@@ -148,7 +176,9 @@ class ProductSeeder extends Seeder
                 'price' => 1599.99,
                 'thumbnail' => 'seeders/products/dell-xps-15.jpg',
                 'category_slug' => 'electronics',
-                'brand_slug' => 'dell'
+                'brand_slug' => 'dell',
+                'is_trending' => true,
+                'is_featured' => false,
             ],
         ];
 
@@ -172,6 +202,8 @@ class ProductSeeder extends Seeder
                             'thumbnail' => $product['thumbnail'],
                             'category_id' => $category->id,
                             'brand_id' => $brand->id,
+                            'is_trending' => $product['is_trending'],
+                            'is_featured' => $product['is_featured'],
                             'created_at' => now(),
                             'updated_at' => now(),
                         ]
