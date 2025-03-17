e64515c07726aa419cdaac78b4771fb04ae013c9
MHRafix <fmhrafiz27@gmail.com>
2025-03-17 22:50:45 +0600

    cart items list done

--- resources/js/themes/BoroBazar/hooks/useCart.ts
+++ resources/js/themes/BoroBazar/hooks/useCart.ts
@@ -1,30 +1,53 @@
-const BASE_URL = "http://127.0.0.1:8000/api/v1";
+// const BASE_URL = "https://ecommerce.test/api/v1";
+
+import { useState } from "react";
+
+export interface CartItemType {
+    id: number;
+    item: string;
+    qty: number;
+    price: number;
+    total: number;
+    product: CartProductType;
+}
+
+export interface CartProductType {
+    id: number;
+    name: string;
+    price: string;
+    image: string;
+}
 
 const useCart = () => {
+    const [cartItems, setCartItems] = useState<CartItemType[]>([]);
+
     const fetchCartItems = async () => {
         try {
             const authToken =
-                "2|3fFVuYCNYyBxH0P4j8HmJKHho4TqP2GvVgUvTIYx889d94f1"; // Replace with actual token
+                "21|3s4Drow5X73hVHkP2pKOnAgP95jclZOvwqV6dJ5Y3974cd32"; // Replace with actual token
 
-            const response = await fetch(`${BASE_URL}/cart-items`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${authToken}`, // ✅ Added token here
-                },
-            });
+            const response = await fetch(
+                `https://ecommerce.test/api/v1/cart-items`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${authToken}`, // ✅ Added token here
+                    },
+                }
+            );
 
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
 
             const data = await response.json();
-            console.log({ fetchItems: data });
+            setCartItems(data?.data?.cart_items);
         } catch (error) {
             console.error("Error fetching cart items:", error);
         }
     };
 
-    return { fetchCartItems, cartItems: [] };
+    return { fetchCartItems, cartItems };
 };
 export default useCart;
