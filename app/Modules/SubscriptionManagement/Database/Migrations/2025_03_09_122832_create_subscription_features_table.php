3d8cf03b221b8def1183749af5218bc00b90c360
Mahadi Hasan <mahadi.dev.pm@gmail.com>
2025-11-20 09:07:09 +0600

    updated

--- app/Modules/SubscriptionManagement/Database/Migrations/2025_03_09_122832_create_subscription_features_table.php
+++ app/Modules/SubscriptionManagement/Database/Migrations/2025_03_09_122832_create_subscription_features_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Models\Subscription;
+use App\Modules\SubscriptionManagement\Models\Subscription;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
